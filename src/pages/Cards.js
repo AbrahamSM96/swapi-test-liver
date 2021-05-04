5461349beaa32514f14cda839bbbb2e18a72cdde
Abraham Serrano <abrahamserrano@MacBook-Pro-de-Abraham-2.local>
2021-05-04 13:21:18 -0500

    delete searcher in cards

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Searcher from 'src/components/Searcher'
 import ContentCard from '../components/ContentCard'
 import NavBar from '../components/NavBar'
 export default function Cards() {
   return (
     <>
       <NavBar />
-      <Searcher />
       <ContentCard />
     </>
   )
